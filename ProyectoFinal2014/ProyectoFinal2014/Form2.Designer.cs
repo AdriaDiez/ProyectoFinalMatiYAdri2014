a3e6a5be5ef01bae5e7599cc706dff279f221940
Adrian Diez <paseomonguis@hotmail.com>
2014-05-30 10:26:33 +0200

    boton volver
    boton volver

--- ProyectoFinal2014/ProyectoFinal2014/Form2.Designer.cs
+++ ProyectoFinal2014/ProyectoFinal2014/Form2.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(103, 117);
+            this.button1.Location = new System.Drawing.Point(62, 168);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(166, 58);
             this.button1.TabIndex = 0;
             this.button1.Text = "volver";
             this.button1.UseVisualStyleBackColor = true;
